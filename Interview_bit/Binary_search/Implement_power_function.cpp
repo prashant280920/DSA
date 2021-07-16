80e3d6889590316da8a8bc104d2e3a1255528491
prashant280920 <b19101@students.iitmandi.ac.in>
2021-07-16 22:05:28 +0530

    Implement Power Function

--- Interview_bit/Binary_search/Implement_power_function.cpp
+++ Interview_bit/Binary_search/Implement_power_function.cpp
@@ -1,41 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long int power(long long int x,long long int n,long long int d){
-    cout << x<<" "<<n<<"\n";
-    if(x==0 && n==0) return 0;
-    if(n==0) return 1;
-    if(n==1) return x%d;
-    long long int temp = power(x,n/2,d);
-    cout << "temp"<<temp<<"\n";
-        if(n%2==0) return ((temp%d)*(temp%d))%d;
-        else return ((x%d)*(temp%d)*(temp%d))%d;
-    
-    
-}
-
 int main(){
     int x,n,d;
     cin >> x >> n >> d;
-    if (x == 0) return 0;
-    if (d == 0) return -1;
-    if (n == 0) return 1;
-long long res = x;
-long long rem_fix = 1;
-while (n > 1)
-{
-
-  if (n % 2 == 1)
-  {
-    rem_fix = (rem_fix * res)%d;
-  }
-  res = ((res%d) * (res%d)) % d;
-  n = n / 2;
-}
-res = (res *rem_fix) % d;
-if (res < 0)
-  res += d;
-return res;
-    
-    return 0;
+    long long int result = 1, x1=x;
+    x1 = x1%d;
+    if(x==0) {
+      cout << "pow("<<x<<","<<n<<")%"<<d<<": "<< 0 << endl;
+      return 0;
+    }
+    if(n==0){
+      cout << "pow("<<x<<","<<n<<")%"<<d<<": "<< 0 << endl;
+      return 1;
+    }
+    if(d==0){
+      cout << "pow("<<x<<","<<n<<")%"<<d<<": "<< 0 << endl;
+      return -1;
+    } 
+    while(n>0){
+      if(n & 1){
+        result = (result*x1)%d;
+      }
+      n = n>>1;
+      x1 = (x1*x1)%d;
+    }
+    if(result>=0){
+      cout << "pow("<<x<<","<<n<<")%"<<d<<": "<< result << endl;
+   
+    }else{
+      cout << "pow("<<x<<","<<n<<")%"<<d<<": "<< d+result << endl;
+ 
+    }
 }
