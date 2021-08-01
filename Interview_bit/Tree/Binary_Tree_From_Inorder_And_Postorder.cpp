f0287a8372bc8a860cf4b0891ab2c2b3f992b2e4
prashant280920 <b19101@students.iitmandi.ac.in>
2021-08-01 22:02:53 +0530

    Binary Tree From Inorder And Postorder

---
+++ Interview_bit/Tree/Binary_Tree_From_Inorder_And_Postorder.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(NULL), right(NULL){}
+};
+
+int searchIndex(vector<int> &B, int start, int end, int val){
+    for(int i=start; i<=end; i++){
+        if(B[i]==val){
+            return i;
+        } 
+    }
+    return -1;
+}
+
+TreeNode* CreateTree(vector<int> &A, vector<int> &B, int start, int end, int &preIndex){
+    if(start>end) return NULL;
+    TreeNode* node = new TreeNode(A[preIndex]);
+    preIndex--;
+    if(start==end) return node;
+    int inOrderIndex = searchIndex(B, start, end, node->val);
+    node->right = CreateTree(A,B, inOrderIndex+1, end, preIndex);
+    node->left = CreateTree(A,B, start, inOrderIndex-1, preIndex);
+    
+    return node;
+
+}
+void inorder(TreeNode* A){
+    if(A==NULL) return;
+    
+    inorder(A->left);
+    cout << A->val<<" ";
+    //ans.push_back(A->val);
+    inorder(A->right);
+}
+int main(){
+    int n, t;
+    cin >> n;
+    vector<int> A, B;
+    cout << "Enter Inorder: ";
+    for(int i=0; i<n; i++){
+        cin >> t;
+        A.push_back(t);
+    }
+    cout << "Enter Postorder: ";
+    for(int i=0; i<n; i++){
+        cin >> t;
+        B.push_back(t);
+    }
+    n = A.size();
+    int preIndex = n-1;
+    TreeNode* ans = CreateTree(B,A, 0,n-1, preIndex);
+    //return ans;
+    cout << "Printing the inorder Traversal of tree : \n";
+    inorder(ans);
+    cout << endl;
+    return 0;
+}
