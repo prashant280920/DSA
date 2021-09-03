62e69e0c45dbd83baca4e1f5f2cb450e8a78fb8c
prashant280920 <b19101@students.iitmandi.ac.in>
2021-09-03 13:36:35 +0530

    Remove Duplicates from Sorted List II

---
+++ Interview_bit/Linked_list/Remove_Duplicates_from_Sorted_List_II.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+    public:
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL){}
+};
+
+void append(ListNode** head_ref, int new_data){
+    ListNode* new_node = new ListNode(new_data);
+    ListNode* last_node = *head_ref;
+
+    if(last_node == NULL){
+        *head_ref = new_node;
+        return;
+    }
+
+    while(last_node->next!=NULL){
+        last_node = last_node->next;
+    }
+    last_node->next = new_node;
+    return;
+}
+
+void print(ListNode* head){
+    while(head!=NULL){
+        cout << head->val<<"->";
+        head = head->next;
+    }
+    cout << "NULL" <<endl;
+}
+
+
+int main(){
+    ListNode* A = NULL;
+    int n,B;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> B;
+        append(&A,B);
+    }
+    //cin >> B;
+    cout << "Input List: ";
+    print(A);
+    if(!A || !A->next){
+        cout << "Output List: \n";
+        print(A);
+        return 0;
+    }
+                
+    ListNode* fakeHead = new ListNode(0);
+    fakeHead->next = A;
+    ListNode* prev = fakeHead;
+    while (A)
+    {
+        while (A->next && A->val == A->next->val)
+            A = A->next;
+        
+        if (prev->next == A)
+            prev = prev->next; 
+        else 
+            prev->next = A->next;
+            
+        A = A->next;
+    }
+    cout << "Output List: \n";
+    print(fakeHead->next);
+
+
+    return 0;
+}
