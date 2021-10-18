faa43ed9e60493ff8a0bfe78015d9e0bd2c8d87a
Prashant Kumar <b19101@students.iitmandi.ac.in>
2021-10-18 16:10:31 +0530

    Update Merge_Sort.cpp

--- DSA/Sort/C++/Merge_Sort.cpp
+++ DSA/Sort/C++/Merge_Sort.cpp
@@ -1,4 +1,8 @@
 
+#include <iostream>
+using namespace std;
+
+// Pseudo Code:
 // MergeSort(arr[], l,  r)
 // If r > l
 //      1. Find the middle point to divide the array into two halves:  
@@ -20,8 +24,8 @@
 // * Merge Sort is useful for sorting linked lists.
 // * Merge Sort is a stable sort which means that the same element in an array maintain their original positions with respect to each other.
 
-#include <iostream>
-using namespace std;
+
+
 void merge(int arr[], int l, int m, int r){
     int n1 = m - l + 1;
     int n2 = r - m;
@@ -81,74 +85,9 @@ int main(){
     for (int i=0; i<n; i++){
         cout << arr[i]<<" ";
     }
-
-    return 0;
     cout<<endl;
-}
-
-
-#include <iostream>
-using namespace std;
-void merge(int arr[], int l, int m, int r){
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1]; int R[n2];
-    for (int i =0; i<n1; i++){
-        L[i] = arr[l + i];
-    }
-    for (int i=0; i<n2; i++){
-        R[i] = arr[m + 1 +i];
-    }
-    int i = 0;
-    int j = 0;
-    int k = l;
-    while (i<n1 and j<n2){
-        if (L[i] <= R[j]){
-            arr[k] = L[i];
-            i++;
-        }
-        else{
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    while(i<n1){
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while(j<n2){
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-
-}
-void mergeSort(int arr[], int l, int r){
-    if (l>=r){
-        return;
-    }
-    else{
-        int m;
-        m = l + (r-l)/2;
-        mergeSort(arr,l,m);
-        mergeSort(arr,m+1,r);
-        merge(arr, l, m, r);
-    }
-}
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i=0; i<n; i++){
-        cin >> arr[i];
-    }
-    mergeSort(arr, 0, n-1);
-    for (int i=0; i<n; i++){
-        cout << arr[i]<<" ";
-    }
-
     return 0;
-    cout<<endl;
+    
 }
+
+
